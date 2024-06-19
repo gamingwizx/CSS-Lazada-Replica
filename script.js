49a40bf0d983f6a5f5e502a90aa2484009f3c78a
gamingwizx <gamingwizx@gmail.com>
2024-06-19 15:14:48 +0800

    Make mobile header stick to the search bar when scrolled pass, and added mobile navigation

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const mobileOptions = document.querySelector(".items-container--options")
 const scrollContainer = document.querySelector(".scroll-container")
 const scroll = document.querySelector(".scroll")
+const mobileHeaders = document.querySelector(".mobile-header")
+const mobileHeaderParent = document.querySelector(".lazada-search")
+const search = document.querySelector(".lazada-search")
+const flashSale = document.querySelector(".item-container--flash-sale")
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      mobileHeaders.style.display = "none"
+      mobileHeaderParent.style.backgroundImage =
+        "url('image/mobile-search-background.webp')"
+    } else {
+      mobileHeaderParent.style.backgroundColor = "white"
+      mobileHeaderParent.style.backgroundImage = "none"
+      mobileHeaders.style.display = "block"
+    }
+  },
+  {
+    threshold: 0,
+    rootMargin: "-73px 0px 0px 0px",
+    root: null
+  }
+)
+observer.observe(flashSale)
 
 // The relative width between the custom scroll bar and the scroll
 const relativeCustomScrollBarAndScrollWidth =
